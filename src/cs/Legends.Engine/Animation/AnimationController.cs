6d7a4229aba5b9b42d8625211f1c759b1670ad77
Richard Woods <me@rfwoods.net>
2024-01-02 21:27:25 -0600

    add scale and rotate animations

--- src/cs/Legends.Engine/Animation/AnimationController.cs
+++ src/cs/Legends.Engine/Animation/AnimationController.cs
@@ -71,6 +71,77 @@ public class TranslateAnimationData : AnimationData
     }
 }
 
+public class ScaleAnimationData : AnimationData
+{
+    public bool Relative    { get; set; }
+    public Vector2 From     { get; set; }
+    public Vector2 To       { get; set; }
+
+    public override void InitializeChannel(AnimationChannel channel) 
+    {
+        var tween = new Tweener()
+                .TweenTo(target: channel.Controller.Parent, 
+                        expression: player => player.Scale, 
+                        toValue: To + (Relative ? channel.Controller.Parent.Scale : Vector2.Zero), 
+                        duration: Duration, 
+                        delay: 0)
+                .Easing(EasingFunctions.Linear);
+        switch(LoopType)
+        {
+            case LoopType.Loop: 
+                tween.Repeat(RepeatCount <= 0 ? -1 : RepeatCount, RepeatDelay);
+                break;
+            case LoopType.Reverse:
+                tween.Repeat(RepeatCount <= 0 ? -1 : RepeatCount, RepeatDelay)
+                     .AutoReverse();
+                break;
+        }
+
+        channel.State = tween;
+    }
+
+    public override void UpdateChannel(AnimationChannel channel, GameTime gameTime) 
+    {
+        ((Tween)channel.State).Update(gameTime.GetElapsedSeconds());
+    }
+}
+
+public class RotateAnimationData : AnimationData
+{
+    public bool Relative    { get; set; }
+    public float From     { get; set; }
+    public float To       { get; set; }
+
+    public override void InitializeChannel(AnimationChannel channel) 
+    {
+        var tween = new Tweener()
+                .TweenTo(target: channel.Controller.Parent, 
+                        expression: player => player.Rotation, 
+                        toValue: To + (Relative ? channel.Controller.Parent.Rotation : 0.0f), 
+                        duration: Duration, 
+                        delay: 0)
+                .Easing(EasingFunctions.Linear);
+        switch(LoopType)
+        {
+            case LoopType.Loop: 
+                tween.Repeat(RepeatCount <= 0 ? -1 : RepeatCount, RepeatDelay);
+                break;
+            case LoopType.Reverse:
+                tween.Repeat(RepeatCount <= 0 ? -1 : RepeatCount, RepeatDelay)
+                     .AutoReverse();
+                break;
+        }
+
+        channel.State = tween;
+    }
+
+    public override void UpdateChannel(AnimationChannel channel, GameTime gameTime) 
+    {
+        ((Tween)channel.State).Update(gameTime.GetElapsedSeconds());
+    }
+}
+
+
 public class AnimationChannel
 {
     public AnimationController Controller { get; protected set; }
