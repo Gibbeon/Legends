39116eefa22639c8c883f0c551a5127ac208abbd
Richard Woods <me@rfwoods.net>
2024-01-08 10:28:39 -0600

    fix for namespace change

--- src/cs/Legends.App/MainGame.cs
+++ src/cs/Legends.App/MainGame.cs
@@ -1,5 +1,6 @@
 ﻿using Legends.Engine;
 using Legends.Engine.Collision;
+using Legends.Engine.Graphics2D;
 using Legends.Engine.Input;
 using Microsoft.Xna.Framework;
 using MonoGame.Extended.Screens;
