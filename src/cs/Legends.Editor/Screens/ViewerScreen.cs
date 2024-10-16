c5d3b20f2ab31fa956315d2b697b6e7dac991553
Richard Woods <me@rfwoods.net>
2024-10-16 12:07:11 -0500

    update but camera stuff is all wrong

--- src/cs/Legends.Editor/Screens/ViewerScreen.cs
+++ src/cs/Legends.Editor/Screens/ViewerScreen.cs
@@ -4,27 +4,53 @@ using Legends.Engine.Input;
 using Legends.Engine;
 using System;
 using Legends.Engine.Content;
+using Microsoft.Xna.Framework.Input;
+using System.Xml.Linq;
 
 namespace Legends.Editor.Screens;
 
 public class ViewerScreen : Screen
 {
+    public float ScrollSpeed { get; set; }
+    public float ZoomFactor { get; set; }
+    public float RotateFactor { get; set; }
+
     private readonly IServiceProvider _services;
     private readonly Ref<Scene> _scene;
     private readonly InputManager _input;
+    private InputCommandSet _commands;
     public ViewerScreen(IServiceProvider services)
     {
         _services = services;
-        _input = new InputManager(_services);
+        _input = new InputManager(services);
         _scene = new Scene(_services);
+        (~_scene).Initialize();
 
-        var so = _services.GetContentManager().GetRef<SceneObject>("Sprites/Fence");
+        var so = _services.GetContentManager().GetRef<SceneObject>("Sprites/Actor");
         (~_scene).AttachChild(so);
+        (~so).Zoom(5.0f);
+
+        ScrollSpeed = 1;
+        ZoomFactor = 2;
+        RotateFactor = MathF.PI / 12;
     }
 
     public override void Initialize()
     {
         (~_scene).Initialize();
+
+        _commands = new InputCommandSet(_services, _input);
+
+        _commands.Add("MOVE_LEFT",    EventType.KeyPressed,    Keys.Left);             
+        _commands.Add("MOVE_RIGHT",   EventType.KeyPressed,    Keys.Right);      
+        _commands.Add("MOVE_UP",      EventType.KeyPressed,    Keys.Up);             
+        _commands.Add("MOVE_DOWN",    EventType.KeyPressed,    Keys.Down);
+        
+        _commands.Add("ZOOM_IN",      EventType.KeyPressed,     Keys.T);           
+        _commands.Add("ZOOM_OUT",     EventType.KeyPressed,    Keys.Y);             
+        _commands.Add("ROTATE",       EventType.KeyPressed,        Keys.R);
+        
+        _commands.Enabled = true;
     }
 
     public override void Draw(GameTime gameTime)
@@ -34,6 +60,22 @@ public class ViewerScreen : Screen
 
     public override void Update(GameTime gameTime)
     {
+        foreach(var command in _commands.EventActions)
+        {
+            switch(command.Name)
+            {
+                case "MOVE_LEFT":   (~_scene).Camera.Move(-ScrollSpeed, 0); break;
+                case "MOVE_RIGHT":  (~_scene).Camera.Move( ScrollSpeed, 0); break;
+                case "MOVE_UP":     (~_scene).Camera.Move( 0,-ScrollSpeed); break;
+                case "MOVE_DOWN":   (~_scene).Camera.Move( 0, ScrollSpeed); break;
+                case "ZOOM_IN":     (~_scene).Camera.Zoom(1 / ZoomFactor); break;
+                case "ZOOM_OUT":    (~_scene).Camera.Zoom(    ZoomFactor); break;
+                case "ROTATE":      (~_scene).Camera.RotateByDegrees(RotateFactor); break;
+                default:
+                    Console.WriteLine("Unknown Command: {0}", command.Name); break;             
+            }
+        }  
+
         (~_scene).Update(gameTime);
     }
 
@@ -42,7 +84,6 @@ public class ViewerScreen : Screen
         GC.SuppressFinalize(this);
 
         (~_scene).Dispose();
-        _input.Dispose();
 
         base.Dispose();
     }
